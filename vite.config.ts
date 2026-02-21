39aa3278bde5c09d5ca0d0dc1e6efeb5c793b735
sp3izy <jpestanabambirra@gmail.com>
2026-02-21 20:42:19 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: "/stratanix-ai/",
+ base: "/Stratanix-ai/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
